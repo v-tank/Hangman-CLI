35ca2ae23cf097cbf596a4fd91a3a1cf42859060
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-20 19:50:02 -0800

    added comments

--- word.js
+++ word.js
@@ -1,23 +1,28 @@
+// Require the 'letter' object
 var Letter = require('./letter.js');
 
+// Word constructor function
 var Word = function(word) {
-  var arrayOfLetters = [];
-  var string = "";
+  var arrayOfLetters = []; // Array to hold multiple 'Letter' objects for each word
+  var string = ""; // An empty string to print the word to the screen
 
+  // Loop to create a new 'Letter' object for each letter in a word
   for (var i = 0 ; i < word.length; i++) {
-    var newLetter = new Letter(word[i]);
-    arrayOfLetters.push(newLetter);
+    var newLetter = new Letter(word[i]); // Create a new object for the specific letter in the word
+    arrayOfLetters.push(newLetter); // Push the object to the empty array
   }
 
+  // Function to print blanks and correct guesses to the screen
   this.printToScreen = function() { 
-    string = "";
+    string = ""; // Empty out the string each time the string is printed
     for (var i=0; i < arrayOfLetters.length; i++) {
-      string += arrayOfLetters[i].printLetter() + " ";
+      string += arrayOfLetters[i].printLetter() + " "; // Forms the string to print to the screen
     }
-    console.log(string);
+    console.log(string); // Logs the string to the screen
     return string;
   }
 
+  // Function to check whether the guessed letter matches an underlying character; if so, reveal it
   this.guess = function(char) {
     for (var i=0; i < arrayOfLetters.length; i++) {
       arrayOfLetters[i].checker(char);
@@ -25,4 +30,5 @@ var Word = function(word) {
   }
 }
 
+// Export the object to be used by other files
 module.exports = Word;
